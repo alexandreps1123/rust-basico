65f740bbd605e932677fe2d6674cda9c641fc6ca
alexandreps1123 <alexandre.ps1123@gmail.com>
2022-11-20 17:54:16 -0300

    versão 1 do jogo da adivinhação

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,30 @@
+//prelude
+use std::io;
+
+//'fn' new function
+fn main() {
+    //'println!' is a macro to print a string
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    //'let' create a variable, variables are immutable by default
+    //'mut' make a variable mutable
+    //'=' bind a value to the variable
+    //'String::new' function that returns a new instance of a string
+    //'::' indicates that 'new' is an associated function of the 'String' type
+    //'new' create a new, empty string
+    let mut guess = String::new();
+
+    //'io::stdin' handle with user input
+    //'read_line' get user input
+    //'&' indicates reference
+    //'&mut guess' make reference mutable, to get user input to change the strin's content
+    //'read_line' returns a 'Result' value. 'Result' is a enumeration (enum)
+    //'expect'  if 'Result' is an 'Err' value, 'expect' will crash the program and display the message
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("Your guessed: {guess}");
+}
