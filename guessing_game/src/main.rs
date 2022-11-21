3f2d366485fa014ad827e51d721ba2b33be3dba7
alexandreps1123 <alexandre.ps1123@gmail.com>
2022-11-20 21:38:41 -0300

    comparação do número gerado com a entrada

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,6 +1,7 @@
 //prelude
-use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 //'fn' new function
 fn main() {
@@ -31,5 +32,13 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number! ");
+    
     println!("Your guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
