5cf725bff627d8009475717b1249f558918a81ff
alexandreps1123 <alexandre.ps1123@gmail.com>
2022-11-20 22:47:32 -0300

    palavra chave 'const'

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,7 +1,21 @@
 fn main() {
+    //const cannot recieve a value computed at runtime
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
     let mut x = 5;
+
+    let y = 5;
+
+    let y = y + 1;
     println!("The value of x is: {x}");
 
+    println!("The const value: {THREE_HOURS_IN_SECONDS}");
+    {
+        let y = y * 2;
+        println!("The value of y is: {y}");
+    }
     x = 6;
     println!("The value of x is: {x}");
+
+    println!("The value of y is: {y}");
 }
