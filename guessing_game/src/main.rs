ad8b2df2cf02978d02429f0b7e8dd08c053ad4f2
alexandreps1123 <alexandre.ps1123@gmail.com>
2022-11-20 21:15:07 -0300

    gerando um número aleatório de 1 até 100.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,10 +1,15 @@
 //prelude
 use std::io;
+use rand::Rng;
 
 //'fn' new function
 fn main() {
     //'println!' is a macro to print a string
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("the secret number is: {secret_number}");
 
     println!("Please input your guess.");
 
