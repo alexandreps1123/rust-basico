364be4ac1c574cef93114efb06c2b7e1981f24b4
alexandreps1123 <alexandre.ps1123@gmail.com>
2022-11-21 11:53:44 -0300

    tipo booleano, e utilização de underscore para variáveis não utilizadas de forma intencional

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -1,5 +1,26 @@
 fn main() {
-    let x = 2.0;    // default f64
+    let _x = 2.0;    // default f64
 
-    let y: f32 = 2.0;
+    let _y: f32 = 2.0;
+
+    // operations
+    // addition
+    let _sum = 5 + 10;
+
+    // subtraction
+    let _difference = 95.5 - 4.3;
+
+    // multiplication
+    let _product = 4 * 30;
+
+    // division
+    let _quotient = 56.6 / 32.2;
+    let _floored = 2 / 3; // 0
+
+    // remainder
+    let _remainder = 43 % 5;
+
+    // boolean
+    let _t = true;
+    let _f: bool = false;
 }
