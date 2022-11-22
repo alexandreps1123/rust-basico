d70075797e41c6451f8a14776b6b8824344d216a
alexandreps1123 <alexandre.ps1123@gmail.com>
2022-11-22 16:52:17 -0300

    multiplos parumetros para un funcao

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -4,6 +4,8 @@ fn main() {
     another_function();
 
     another_function_with_parameters(5);
+
+    multiples_paramenters(10, 'h');
 }
 
 fn another_function() {
@@ -12,4 +14,8 @@ fn another_function() {
 
 fn another_function_with_parameters(x: i32) {
     println!("Value parameter {x}");
+}
+
+fn multiples_paramenters(x: i32, letter: char) {
+    println!("Value parameter {x}{letter}");
 }
