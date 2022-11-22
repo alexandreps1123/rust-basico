a2c0f425b082d5ce53e6c7e5f9964d842dc1e6e7
alexandreps1123 <alexandre.ps1123@gmail.com>
2022-11-22 16:44:57 -0300

    função com parametro

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -2,8 +2,14 @@ fn main() {
     println!("Hello, world!");
 
     another_function();
+
+    another_function_with_parameters(5);
 }
 
 fn another_function() {
     println!("Another function!");
+}
+
+fn another_function_with_parameters(x: i32) {
+    println!("Value parameter {x}");
 }
