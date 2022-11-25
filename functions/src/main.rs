a9f360087e25d24a846e9a9891261cd87d06a980
alexandreps1123 <alexandre.ps1123@gmail.com>
2022-11-25 10:35:02 -0300

    retornando valores de funcoes.

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,11 +1,19 @@
 fn main() {
+    let x = six();
+    let y = increment_one(9);
+
     println!("Hello, world!");
+
+    println!("six() return -> {x}");
+
+    println!("increment_one(9) return -> {y}");
 
     another_function();
 
     another_function_with_parameters(5);
 
     multiples_paramenters(10, 'h');
+
 }
 
 fn another_function() {
@@ -18,4 +26,12 @@ fn another_function_with_parameters(x: i32) {
 
 fn multiples_paramenters(x: i32, letter: char) {
     println!("Value parameter {x}{letter}");
+}
+
+fn six() -> i32 {
+    6
+}
+
+fn increment_one(x: i32) -> i32 {
+    x + 1
 }
