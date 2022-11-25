7f0b0277e8a75df81c4bd430d557e59a34e8ff84
alexandreps1123 <alexandre.ps1123@gmail.com>
2022-11-25 11:22:34 -0300

    multiplos loops com label e condicao de parada para o loop mais externo

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     let mut counter = 0;
+    let mut i = 0;
 
     let result = loop {
         counter += 1;
@@ -9,7 +10,29 @@ fn main() {
         }
     };
 
-    println!("The result is {result}");
+    println!("The result is : {result}");
+
+    'counting_up: loop {
+        println!("i = {i}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if i == 2 {
+                break 'counting_up;
+            }
+
+            remaining -= 1;
+        }
+
+        i += 1;
+    }
+
+    println!("i = {i}");
+
     //infinite loop
     //loop {
     //    println!("Hello, world!");
