1e8c7e77e999b4608c5defd95614a39267a736bb
alexandreps1123 <alexandre.ps1123@gmail.com>
2022-11-25 11:37:10 -0300

    uso de for e while

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     let mut counter = 0;
     let mut i = 0;
+    let a = [10, 20, 30, 40, 50];
 
     let result = loop {
         counter += 1;
@@ -39,6 +40,25 @@ fn main() {
 
         i -= 1;
     }
+
+    //while loop in an array
+    while i < 5 {
+        println!("value: {}", a[i]);
+
+        i += 1;
+    }
+
+    //for loop
+    for element in a {
+        println!("for element: {element}");
+    }
+
+    //reverse the range
+    for number in (1..5).rev() {
+        println!("{number}");
+    }
+
+
     //infinite loop
     //loop {
     //    println!("Hello, world!");
