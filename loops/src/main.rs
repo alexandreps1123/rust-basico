9618048727111b628c907f8849c21fc041d8edb2
alexandreps1123 <alexandre.ps1123@gmail.com>
2022-11-25 11:14:00 -0300

    loop retornando um valor

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,6 +1,17 @@
 fn main() {
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 11;
+        }
+    };
+
+    println!("The result is {result}");
     //infinite loop
-    loop {
-        println!("Hello, world!");
-    }
+    //loop {
+    //    println!("Hello, world!");
+    //}
 }
